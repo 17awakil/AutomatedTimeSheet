328d2c2863c3bc8018a4ec0c5b783eaae3aa527f
17awakil <andre.wakil@mail.mcgill.ca>
2019-07-05 11:37:32 -0400

    Added a validation check and removed some dead code

--- automated_time_sheet.py
+++ automated_time_sheet.py
@@ -10,8 +10,14 @@ from jira import JIRA
 #Log into jira admin account on server
 jira = JIRA('http://jira:8080', basic_auth=('awakil', 'Nairy444@'))
 
-def issues_by_user_on_date(date):
-    issues = jira.search_issues("project = TEST123 AND status WAS 'In Progress' ON {}".format(date), expand = "changelog")
+#Validation check
+if len(sys.argv) < 2:
+    print("Must input the date as a command line argument in the following format YYYY-MM-DD")
+    sys.exit()
+
+#Get issues worked on by date
+def issues_by_date(date):
+    issues = jira.search_issues("project = TEST123 AND status WAS 'In Progress' ON {}".format(date))
     users_issues = {}
     for issue in issues:
         if issue.fields.assignee.displayName not in users_issues.keys():
@@ -19,10 +25,10 @@ def issues_by_user_on_date(date):
         users_issues[issue.fields.assignee.displayName].append(issue.key)
     return users_issues
 
-
+#Write data into csv file
 with open("auto_time_report.csv", "w", newline = '') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["Automated Monthly Report"])
+    csv_writer.writerow(["Automated JIRA Time Report"])
     csv_writer.writerow(["User"] + ["Assigned Issues"])
-    for user, assigned_issues in issues_by_user_on_date(sys.argv[1]).items():
+    for user, assigned_issues in issues_by_date(sys.argv[1]).items():
             csv_writer.writerow([user] + [assigned_issues])
