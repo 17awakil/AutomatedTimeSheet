0dc9085d1a3cd06f8c542a9d065b234a2fffaeb1
17awakil <andre.wakil@mail.mcgill.ca>
2019-07-11 14:33:40 -0400

    Made changes requested

--- automated_time_sheet.py
+++ automated_time_sheet.py
@@ -16,23 +16,25 @@ if len(sys.argv) < 2:
 #Log into jira admin account on server
 jira = JIRA('http://jira:8080', basic_auth=('awakil', 'Nairy444@'))
 
+#Constant declaration
 date = sys.argv[1]
+project = "TEST123"
 
 #Get users from jira server that are in a specific project
-users = jira.search_assignable_users_for_projects("", "TEST123")
+users = jira.search_assignable_users_for_projects("", project)
 
 #Process issues in the following data structure: {user1 : [ {issue1 : hours1}, {issue2 : hours2}, ...], user2 : [{issue3 : hours3}, ...], ...}
 user_issues = {}
 for user in users:
-    user_issues[user.displayName] = []
-    for issue in jira.search_issues("project = TEST123 AND status WAS 'In Progress' ON {} AND assignee WAS '{}' ON {} ".format(date, user.key, date)):
-        user_issues[user.displayName].append({issue.key : 0 })
+    user_issues[user.displayName] = {}
+    for issue in jira.search_issues("project = {} AND status WAS 'In Progress' ON {} AND assignee WAS '{}' ON {} ".format(project, date, user.key, date)):
+        user_issues[user.displayName][issue.key] = 0
 
 
 #Calculate hours spent on issues (high-level estimation)
 for user, assigned_issues in user_issues.items():
-    for issue in assigned_issues:
-        issue[list(issue.keys())[0]] = 8.0 / len(assigned_issues)
+    for issue, hours in assigned_issues.items():
+        user_issues[user][issue] = 8.0 / len(assigned_issues.keys())
 
 
 #Write data into csv file
@@ -42,7 +44,6 @@ with open("auto_time_report.csv", "w", newline = '') as csv_file:
     csv_writer.writerow(" ")
     csv_writer.writerow(["User"] + ["Assigned Issues"] + ["Hours Spent"])
     for user, assigned_issues in user_issues.items():
-        for issue in assigned_issues:
-            for issue_key, hours_spent in issue.items():
-                csv_writer.writerow([user] + [issue_key] + [hours_spent])
+        for issue, hours in assigned_issues.items():
+                csv_writer.writerow([user] + [issue] + [hours])
 
