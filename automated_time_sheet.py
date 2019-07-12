43726cff641619fc147bde71acaacb0a2668c3d4
17awakil <andre.wakil@mail.mcgill.ca>
2019-07-12 10:56:20 -0400

    [RD-1202] Multiple days report

--- automated_time_sheet.py
+++ automated_time_sheet.py
@@ -16,18 +16,29 @@ if len(sys.argv) < 2:
 #Log into jira admin account on server
 jira = JIRA('http://jira:8080', basic_auth=('awakil', 'Nairy444@'))
 
+
 #Constant declaration
-date = sys.argv[1]
-project = "TEST123"
+date1 = sys.argv[1]
+date2 = ""
+project_key = "TEST123"
+issues = []
+
+
 
 #Get users from jira server that are in a specific project
-users = jira.search_assignable_users_for_projects("", project)
+users = jira.search_assignable_users_for_projects("", project_key)
 
 #Process issues in the following data structure: {user1 : {issue1 : hours1 , issue2 : hours2} , user2 : {issue3 : hours3}, ...}
 user_issues = {}
 for user in users:
     user_issues[user.displayName] = {}
-    for issue in jira.search_issues("project = {} AND status WAS 'In Progress' ON {} AND assignee WAS '{}' ON {} ".format(project, date, user.key, date)):
+    issues = jira.search_issues("project = {} AND status WAS 'In Progress' ON {} AND assignee WAS '{}' ON {} ".format(project_key, date1, user.key, date1))
+    
+    if len(sys.argv) == 3:
+        date2 = sys.argv[2]
+        issues = jira.search_issues("project = {} AND status WAS 'In Progress' DURING ({},{}) AND assignee WAS '{}' DURING ({},{}) ".format(project_key, date1, date2, user.key, date1, date2))
+    
+    for issue in issues:
         user_issues[user.displayName][issue.key] = 0
 
 
@@ -40,7 +51,12 @@ for user, assigned_issues in user_issues.items():
 #Write data into csv file
 with open("auto_time_report.csv", "w", newline = '') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["Automated JIRA Time Report"] + ["for " + date])
+
+    if len(sys.argv) == 2:
+        csv_writer.writerow(["Automated JIRA Time Report"] + ["for " + date1])
+    elif len(sys.argv) ==3:
+        csv_writer.writerow(["Automated JIRA Time Report"] + ["for " + date1 + " to " + date2]) 
+
     csv_writer.writerow(" ")
     csv_writer.writerow(["User"] + ["Assigned Issues"] + ["Hours Spent"])
     
