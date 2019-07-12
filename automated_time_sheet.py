061242adb3896dcd741664f21ad9d0949cfd7866
17awakil <andre.wakil@mail.mcgill.ca>
2019-07-12 16:16:09 -0400

    Made requested changes

--- automated_time_sheet.py
+++ automated_time_sheet.py
@@ -13,30 +13,36 @@ if len(sys.argv) < 2:
     sys.exit("Must input the date as a command line argument in the following format: YYYY-MM-DD")
 
 
-#Log into jira admin account on server
-jira = JIRA('http://jira:8080', basic_auth=('awakil', 'Nairy444@'))
+
+
+def hours_spent(issue):
+    pass
 
 
 #Constant declaration
-date1 = sys.argv[1]
-date2 = ""
-project_key = "TEST123"
+PROJECT_KEY = "TEST123"
+
+#Global variables
+start_date = sys.argv[1]
+end_date = ""
 issues = []
 
-
+#Log into jira admin account on server
+jira = JIRA('http://jira:8080', basic_auth=('awakil', 'Nairy444@'))
 
 #Get users from jira server that are in a specific project
-users = jira.search_assignable_users_for_projects("", project_key)
+users = jira.search_assignable_users_for_projects("", PROJECT_KEY)
 
 #Process issues in the following data structure: {user1 : {issue1 : hours1 , issue2 : hours2} , user2 : {issue3 : hours3}, ...}
 user_issues = {}
 for user in users:
     user_issues[user.displayName] = {}
-    issues = jira.search_issues("project = {} AND status WAS 'In Progress' ON {} AND assignee WAS '{}' ON {} ".format(project_key, date1, user.key, date1))
     
-    if len(sys.argv) == 3:
-        date2 = sys.argv[2]
-        issues = jira.search_issues("project = {} AND status WAS 'In Progress' DURING ({},{}) AND assignee WAS '{}' DURING ({},{}) ".format(project_key, date1, date2, user.key, date1, date2))
+    if len(sys.argv) == 2:
+        issues = jira.search_issues("project = {} AND status WAS 'In Progress' ON {} AND assignee WAS '{}' ON {} ".format(PROJECT_KEY, start_date, user.key, start_date))
+    elif len(sys.argv) == 3:
+        end_date = sys.argv[2]
+        issues = jira.search_issues("project = {} AND status WAS 'In Progress' DURING ({},{}) AND assignee WAS '{}' DURING ({},{}) ".format(PROJECT_KEY, start_date, end_date, user.key, start_date, end_date))
     
     for issue in issues:
         user_issues[user.displayName][issue.key] = 0
@@ -44,7 +50,7 @@ for user in users:
 
 #Calculate hours spent on issues (high-level estimation)
 for user, assigned_issues in user_issues.items():
-    for issue, hours in assigned_issues.items():
+    for issue in assigned_issues:
         user_issues[user][issue] = 8.0 / len(assigned_issues.keys())
 
 
@@ -53,9 +59,9 @@ with open("auto_time_report.csv", "w", newline = '') as csv_file:
     csv_writer = csv.writer(csv_file)
 
     if len(sys.argv) == 2:
-        csv_writer.writerow(["Automated JIRA Time Report"] + ["for " + date1])
+        csv_writer.writerow(["Automated JIRA Time Report"] + ["for " + start_date])
     elif len(sys.argv) ==3:
-        csv_writer.writerow(["Automated JIRA Time Report"] + ["for " + date1 + " to " + date2]) 
+        csv_writer.writerow(["Automated JIRA Time Report"] + ["from " + start_date + " to " + end_date]) 
 
     csv_writer.writerow(" ")
     csv_writer.writerow(["User"] + ["Assigned Issues"] + ["Hours Spent"])
